0a8791a38a6f3679c8522c06d60562c2b0f0c4cd
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-19 19:47:13 +0200

    solution

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+
+  const navigate = useNavigate();
+
+  return (
+  <>
+    <h1>Contact</h1>
+    <button onClick={()=>navigate("/Home")}>Submit</button>
+  </>
+  )
 };
 
 export default Contact;
